4a3631138a316bbd1e484a8bde37fefa72f3300b
Allison Durkan <allisondurkan@gmail.com>
2025-06-30 12:47:37 -0400

    updated output to match the guidelines

--- Program.cs
+++ Program.cs
@@ -4,15 +4,14 @@
     {
         static void Main(string[] args)
         {
-            // Create elevator system
-            ElevatorSimulator elevatorSystem = new ElevatorSimulator();
+            // Create elevator simulator
+            ElevatorSimulator elevatorSimulator = new ElevatorSimulator();
 
-            // Scenario 1: Basic Test Case from Problem Description
             // At time 0, a user on floor 3 presses the "Down" request button. Their destination is floor 2.
-            elevatorSystem.AddElevatorCall(new ElevatorCall(3, 2, Direction.Down, 0));
+            elevatorSimulator.AddElevatorCall(new ElevatorCall(3, 2, Direction.Down, 0));
 
             // At time 1, a user on floor 10 presses the "Down" request button. Their destination is floor 1.
-            elevatorSystem.AddElevatorCall(new ElevatorCall(10, 1, Direction.Down, 1));
+            elevatorSimulator.AddElevatorCall(new ElevatorCall(10, 1, Direction.Down, 1));
 
             // Generate and add 10 random requests
             /*var randomRequests = RandomRequestGenerator.GenerateRandomElevatorRequests(10);
@@ -23,11 +22,11 @@
                                 $"presses the \"{request.CallDirection}\" request button. " +
                                 $"Their destination is floor {request.DestinationFloor}.");
 
-                elevatorSystem.AddElevatorCall(request);
+                elevatorSimulator.AddElevatorCall(request);
             }*/
 
             // Run the simulation
-            elevatorSystem.Simulate();
+            elevatorSimulator.Simulate();
         }
     }
 }
